713aac56823f4de538ff2fddc79db2fdc626393c
Alvaro Centeno <alvaroc0689@gmail.com>
2020-06-10 15:24:50 -0700

    added prompt questions to app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,3 +1,4 @@
+const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -9,6 +10,96 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+
+const emp = [];
+const man = [];
+const eng = [];
+const int = [];
+
+let id = 0;
+
+newHire();
+
+function newHire() {
+    inquirer.prompt([
+        {
+            type: 'text',
+            name: 'username',
+            message: 'What is your username?'
+        },
+        {
+            type: 'password',
+            name: 'value',
+            message: 'what is your password?'
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is the new hire's role?",
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            messgae: 'New Hire Name:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'New Hire Email:'
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: 'New Hire Office Number:',
+            when: (answers) => answers.role === 'Manager'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'New Hire Github Username:',
+            when: (answers) => answers.role === 'Engineer'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'New Hire University:',
+            when: (answers) => answers.role === 'Intern'
+        }
+
+    ]).then(res => {
+        if (res.type === 'Manager') {
+            man.push(new Manager(es.name, id, res.email, res.office));
+            id++
+        } else if (res.type === 'Engineer') {
+            eng.push(new Engineer(res.name, id, res.email, res.github));
+            id++
+        } else if (res.type === 'Intern') {
+            int.push(new Intern(res.name, id, res.email, res.school));
+            id++
+        };
+
+    })
+    //     .catch(err => console.log(err);
+    // )
+}
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'another',
+        message: 'Add another new hire?',
+        choices: ['Yes', 'No']
+    }
+]).then(res => {
+    if (res.choices === 'Yes') {
+        newHire();
+    } else {
+        console.log('New Hire Complete!');
+
+    }
+})
+
 
 
 // Write code to use inquirer to gather information about the development team members,
